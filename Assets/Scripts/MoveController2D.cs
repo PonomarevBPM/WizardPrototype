870a66ef5e3c1bf68df6b97730197a6265499823
PonomarevAA <ponomarev.bpm@gmail.com>
2022-04-18 16:14:08 +0300

    Добавил что-то

--- Assets/Scripts/MoveController2D.cs
+++ Assets/Scripts/MoveController2D.cs
@@ -14,30 +14,13 @@ namespace Wizard2D
 		private float horizontalDirection;
 		private Rigidbody2D body;
 		private bool jump;
+		private bool canJumping;
 
 		void Start()
 		{
 			body = GetComponent<Rigidbody2D>();
 			
 			
-		}
-
-		void OnCollisionStay2D(Collision2D coll)
-		{
-			//Проверка на земле ли игрок
-			if (coll.transform.tag == "Ground")
-			{
-				
-				jump = true;
-			}
-		}
-
-		void OnCollisionExit2D(Collision2D coll)
-		{
-			if (coll.transform.tag == "Ground")
-			{
-				jump = false;
-			}
 		}
 
 		void FixedUpdate()
@@ -47,7 +30,10 @@ namespace Wizard2D
 
 			body.velocity = new Vector2(horizontalDirection * speed * Time.deltaTime, 0f);
 
-			if (Input.GetButton("Jump") && jump)
+			canJumping = GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+
+			if (Input.GetButton("Jump") && canJumping)
 				{
 					body.velocity = new Vector2(0, addForce);
 				}
