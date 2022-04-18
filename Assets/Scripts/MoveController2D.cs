0140ad5cc0597e7ecdec5cd240115f154b91cee7
PonomarevAA <ponomarev.bpm@gmail.com>
2022-04-18 11:49:30 +0300

    Добавил спрайты игрока, зомби и земляных платформ. Так же решил за пушить скрипт,, который вчера вместе переделали, если че откатим

--- Assets/Scripts/MoveController2D.cs
+++ Assets/Scripts/MoveController2D.cs
@@ -5,46 +5,29 @@ using UnityEngine;
 
 namespace Wizard2D
 {
-	[RequireComponent(typeof(Rigidbody2D))]
+		[RequireComponent(typeof(Rigidbody2D))]
 
 	public class MoveController2D : MonoBehaviour
 	{
-
-		public enum ProjectAxis { onlyX = 0, xAndY = 1 };
-		public ProjectAxis projectAxis = ProjectAxis.onlyX;
-		public float speed = 150;
-		public float addForce = 7;
-		public bool lookAtCursor;
-		public KeyCode leftButton = KeyCode.A;
-		public KeyCode rightButton = KeyCode.D;
-		public KeyCode upButton = KeyCode.W;
-		public KeyCode downButton = KeyCode.S;
-		public KeyCode addForceButton = KeyCode.Space;
-		public bool isFacingRight = true;
-		private Vector3 direction;
-		private float vertical;
-		private float horizontal;
+		[SerializeField]
+		private float speed, addForce;
+		private float horizontalDirection;
 		private Rigidbody2D body;
-		private float rotationY;
 		private bool jump;
 
 		void Start()
 		{
 			body = GetComponent<Rigidbody2D>();
-			body.fixedAngle = true;
-
-			if (projectAxis == ProjectAxis.xAndY)
-			{
-				body.gravityScale = 0;
-				body.drag = 10;
-			}
+			
+			
 		}
 
 		void OnCollisionStay2D(Collision2D coll)
 		{
+			//Проверка на земле ли игрок
 			if (coll.transform.tag == "Ground")
 			{
-				body.drag = 10;
+				
 				jump = true;
 			}
 		}
@@ -53,74 +36,33 @@ namespace Wizard2D
 		{
 			if (coll.transform.tag == "Ground")
 			{
-				body.drag = 0;
 				jump = false;
 			}
 		}
 
 		void FixedUpdate()
 		{
-			body.AddForce(direction * body.mass * speed);
+	
+			horizontalDirection = Input.GetAxisRaw("Horizontal");
 
-			if (Mathf.Abs(body.velocity.x) > speed / 100f)
-			{
-				body.velocity = new Vector2(Mathf.Sign(body.velocity.x) * speed / 100f, body.velocity.y);
-			}
+			body.velocity = new Vector2(horizontalDirection * speed * Time.deltaTime, 0f);
 
-			if (projectAxis == ProjectAxis.xAndY)
-			{
-				if (Mathf.Abs(body.velocity.y) > speed / 100f)
-				{
-					body.velocity = new Vector2(body.velocity.x, Mathf.Sign(body.velocity.y) * speed / 100f);
-				}
-			}
-			else
-			{
-				if (Input.GetKey(addForceButton) && jump)
+			if (Input.GetButton("Jump") && jump)
 				{
 					body.velocity = new Vector2(0, addForce);
 				}
-			}
+			Flip();
 		}
 
-		void Flip()
+		void Flip()	
 		{
-			if (projectAxis == ProjectAxis.onlyX)
-			{
-				isFacingRight = !isFacingRight;
-				Vector3 theScale = transform.localScale;
-				theScale.x *= -1;
-				transform.localScale = theScale;
+			if (body.velocity.x > 0) {
+				transform.localScale =  new Vector2(1, transform.localScale.y);
+			} else if (body.velocity.x < 0) {
+				transform.localScale =  new Vector2(-1, transform.localScale.y);
 			}
 		}
 
-		void Update()
-		{
-			if (lookAtCursor)
-			{
-				Vector3 lookPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
-				lookPos = lookPos - transform.position;
-				float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
-				transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-			}
-
-			if (Input.GetKey(upButton)) vertical = 1;
-			else if (Input.GetKey(downButton)) vertical = -1; else vertical = 0;
-
-			if (Input.GetKey(leftButton)) horizontal = -1;
-			else if (Input.GetKey(rightButton)) horizontal = 1; else horizontal = 0;
-
-			if (projectAxis == ProjectAxis.onlyX)
-			{
-				direction = new Vector2(horizontal, 0);
-			}
-			else
-			{
-				if (Input.GetKeyDown(addForceButton)) speed += addForce; else if (Input.GetKeyUp(addForceButton)) speed -= addForce;
-				direction = new Vector2(horizontal, vertical);
-			}
-
-			if (horizontal > 0 && !isFacingRight) Flip(); else if (horizontal < 0 && isFacingRight) Flip();
-		}
 	}
 }
+
